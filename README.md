267851f0ecc4dce35053b0a79528d985b237ccdd
Ben Blazke <benjiaming@users.noreply.github.com>
2025-04-26 02:18:17 +0000

    updated ImageCarousel

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Implementing various common React components from scratch using different React 
 * [Carousel](carousel/src/app/ImageCarousel.tsx): NextJS
 * [Debounced Input](debounced-input/src/routes/index.tsx): TanStack Router 
 * [Infinite Scroll](infinite-scroll/src/InfiniteScroll.tsx): Bun.sh
-* [Modal-Dialog](modal-dialog/src/ModalDialog.tsx): parcel
-* [Tabs](tabs/app/tabs/tabs.tsx): React Router
+* [Modal-Dialog](modal-dialog/src/ModalDialog.tsx): parcel https://react-components-yzgm.vercel.app/
+* [Tabs](tabs/app/tabs/tabs.tsx): React Router https://react-components-eta-one.vercel.app/
 * [Timers](timers/components/Timers.tsx): Expo
--- carousel/src/app/ImageCarousel.tsx
+++ carousel/src/app/ImageCarousel.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react';
 import './ImageCarousel.css';
 
 export interface ImagesType {
@@ -7,37 +7,56 @@ export interface ImagesType {
   alt: string;
 }
 
-export default function ImageCarousel({ images }: {images: ImagesType[]}) {
-  const [currentImage, setCurrentImage] = useState(0)
+export default function ImageCarousel({ images }: { images: ImagesType[] }) {
+  const [currentImage, setCurrentImage] = useState(0);
+
   const nextImage = () => {
-    setCurrentImage(prev => (prev + 1) % images.length);
+    setCurrentImage((prev) => (prev + 1) % images.length);
   };
 
   const prevImage = () => {
-    setCurrentImage(prev => (prev - 1 + images.length) % images.length);
+    setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
   };
 
   useEffect(() => {
-  const preloadedImages = images.map((i) => {
-        const img = new Image();
-        img.src = i.src;
-        return img;
-      });
-      return () => {
-        preloadedImages.length = 0;
-      };
-    }, [images]);
+    const preloadedImages = images.map((i) => {
+      const img = new Image();
+      img.src = i.src;
+      return img;
+    });
+    return () => {
+      preloadedImages.length = 0;
+    };
+  }, [images]);
 
   return (
-     <div className="carousel-images" style={{ backgroundImage: `url(${images[currentImage].src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div
+      className="carousel-images"
+      style={{
+        backgroundImage: `url(${images[currentImage].src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <div className="arrow" role="button" aria-label="Previous" onClick={prevImage}>
         <span className="material-symbols-outlined">arrow_back</span>
       </div>
       <div className="middle">
-        {images.map((_: any, i: number) => <span key={i} aria-label={`Go to image ${i + 1}`} onClick={()=>setCurrentImage(i)} className={i===currentImage ? "dot dot-current": "dot"}>⋅</span>)}
+        {useMemo(() =>
+          images.map((image: ImagesType, i: number) => (
+            <span
+              key={i}
+              aria-label={`Go to image ${i + 1}`}
+              onClick={() => setCurrentImage(i)}
+              className={i === currentImage ? 'dot dot-current' : 'dot'}
+            >
+              ⋅
+            </span>
+          )),
+        [images, currentImage])}
       </div>
       <div className="arrow" role="button" aria-label="Next" onClick={nextImage}>
-      <span className="material-symbols-outlined">arrow_forward</span>
+        <span className="material-symbols-outlined">arrow_forward</span>
       </div>
     </div>
   );
